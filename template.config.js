351a0737a24c3825a755157658f1651bbddff290
Jose Marmolejos <josemarmolejos@xennialconsulting.com>
2022-01-10 20:29:58 -0500

    New template project name

--- template.config.js
+++ template.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   // Placeholder name that will be replaced in 
   // package.json, index.json, android/, ios/ for a project name.
-  placeholderName: "minimalproject",
+  placeholderName: "MinimalProjectApp",
 
   // Placeholder title that will be replaced in values.xml 
   // and Info.plist with title provided by the user.
-  titlePlaceholder: 'Minimal Project',
+  titlePlaceholder: 'Minimal Project App',
 
   // Directory with template
   templateDir: "./template",
--- template/ios/MinimalProjectAppTests/minimalprojectTests.m
+++ template/ios/MinimalProjectAppTests/minimalprojectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface minimalprojectTests : XCTestCase
+@interface MinimalProjectTests : XCTestCase
 
 @end
 
-@implementation minimalprojectTests
+@implementation MinimalProjectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
